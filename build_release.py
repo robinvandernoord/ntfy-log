016153d12c884958d77a8a32850c5a53ccc0fa5b
Robin van der Noord <robinvandernoord@gmail.com>
2024-04-02 20:23:58 +0200

    feat: translated build_release.sh to build_release.py, update json manifest of available binaries

---
+++ build_release.py
@@ -0,0 +1,54 @@
+import json
+import os
+import tempfile
+from pathlib import Path
+import tomllib # requires Python3.11+
+
+# see `rclone config` -> garage-s3-ntfy-log
+RCLONE_ENDPOINT = "garage-s3-ntfy-log"
+BINARY_NAME = "ntfy-log"
+BUCKET_NAME = "ntfy-log"
+
+
+def is_empty(f: Path) -> bool:
+    return f.stat().st_size == 0
+
+
+def read_cargo():
+    with open("Cargo.toml", "rb") as f:
+        data = tomllib.load(f)
+
+    return data["package"]
+
+
+def main():
+    targets = [
+        "x86_64-unknown-linux-gnu",
+        "aarch64-unknown-linux-gnu",
+    ]
+
+    for target in targets:
+        exit_code = os.system(f"cargo build --release --target {target}")
+        if exit_code:
+            exit(exit_code)
+
+    # separate loop to make sure none of the targets crashed
+    for target in targets:
+        arch = target.split("-")[0]
+        binary_path = f"./target/{target}/release/{BINARY_NAME}"
+        os.system(f"rclone copy --progress {binary_path} {RCLONE_ENDPOINT}:{BUCKET_NAME}/{arch}/")
+
+    # update manifest:
+    with tempfile.NamedTemporaryFile(suffix="build-release.json") as f:
+        os.system(f"rclone copyto --progress {RCLONE_ENDPOINT}:{BUCKET_NAME}/index.json {f.name}")
+
+        f_path = Path(f.name)
+        current_contents = json.loads(f_path.read_text()) if (f_path.exists() and not is_empty(f_path)) else {}
+        current_contents[BINARY_NAME] = read_cargo()
+        f_path.write_text(json.dumps(current_contents))
+
+        os.system(f"rclone copyto --progress {f.name} {RCLONE_ENDPOINT}:{BUCKET_NAME}/index.json")
+        # update manifest at download.s3.su6.nl
+
+if __name__ == "__main__":
+    main()
--- build_release.sh
+++
@@ -1,9 +0,0 @@
-# cargo build --release --target x86_64-unknown-linux-gnu
-# cargo build --release --target aarch64-unknown-linux-gnu
-
-# see `rclone config` -> garage-s3-ntfy-log
-
-rclone copy --progress target/x86_64-unknown-linux-gnu/release/ntfy-log garage-s3-ntfy-log:ntfy-log/x86_64/
-rclone copy --progress target/aarch64-unknown-linux-gnu/release/ntfy-log garage-s3-ntfy-log:ntfy-log/aarch64/
-
-# e.g. https://download.s3.su6.nl/aarch64/ntfy-log
